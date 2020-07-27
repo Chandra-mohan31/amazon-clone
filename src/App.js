d12444e01c3115980645dc9001b09d3a9b00b9cb
Chandra Mohan.S <chandrashanmugam007@gmail.com>
2020-07-27 12:02:11 +0530

    nav bar

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+       <div className="app">
+       <Switch>
+         <Route path= "/checkout">
+           <h1>checkout</h1>
+         </Route>
+         <Route path= "/login">
+           <h1>login</h1>
+         </Route>
+         <Route path= "/">
+           <Header/>
+           <h1>Home page</h1>
+           
+
+           
+             
+           
+         </Route>
+       </Switch>
+       </div>
+    </Router>
+   
+  );
+}
+
+export default App;
